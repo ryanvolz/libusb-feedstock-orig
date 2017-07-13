70f5c913fb5fb71d90c5bb8ff72a725c43b6947f
Ryan Volz <rvolz@mit.edu>
2017-07-13 16:41:36 -0400

    Initial feedstock commit with conda-smithy 2.3.2.

---
+++ recipe/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source activate "${CONDA_DEFAULT_ENV}"
+
+# make builds with gcc>=5 compatible with conda-forge, currently using gcc<5
+CXXFLAGS="${CXXFLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
+
+mkdir build
+cd build
+../configure --prefix=${PREFIX}
+make
+make check
+make install
