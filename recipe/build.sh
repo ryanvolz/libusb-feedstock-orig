8618bb15468e6d73bf6df305f892bcc962eced38
Ryan Volz <rvolz@mit.edu>
2019-06-10 15:52:01 -0400

    Re-enable CXX11 support as it works with modern conda-forge. [skip ci]

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# make builds with gcc>=5 compatible with conda-forge, currently using gcc<5
-CXXFLAGS="${CXXFLAGS} -D_GLIBCXX_USE_CXX11_ABI=0"
-
 mkdir build
 cd build
 ../configure --prefix=${PREFIX} --disable-udev
